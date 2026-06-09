f561fe8391f8171e116b18f68b745e432647e70a
KamranPA <155093978+KamranPA@users.noreply.github.com>
2026-06-09 10:00:36 +0330

    Delete check_fs.py

--- check_fs.py
+++
@@ -1,8 +0,0 @@
-import os
-try:
-    os.makedirs("data", exist_ok=True)
-    with open("data/test_file.txt", "w") as f:
-        f.write("test")
-    print("✅ دسترسی نوشتن وجود دارد و پوشه ساخته شد.")
-except Exception as e:
-    print(f"❌ خطا در دسترسی: {e}")
