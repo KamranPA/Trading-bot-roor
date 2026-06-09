fdee3ce750b5fcb164b95c3bea8645adc2180529
KamranPA <155093978+KamranPA@users.noreply.github.com>
2026-06-10 02:49:50 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
-import sys, os
-# این خط باعث می‌شود مسیر src همیشه در دسترس پایتون باشد
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
+# ---------------------------------------------------------
+# FILE PATH: src/__init__.py
+# ---------------------------------------------------------
 from . import database, coinex_client, strategy, telegram_bot, indicators, train_model, optimizer, backtester
+
+__all__ = ['database', 'coinex_client', 'strategy', 'telegram_bot', 'indicators', 'train_model', 'optimizer', 'backtester']
