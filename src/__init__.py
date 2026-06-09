a21ffe0218f4745a91d3ec0dac6b6612a5837b86
KamranPA <155093978+KamranPA@users.noreply.github.com>
2026-06-10 02:54:35 +0330

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,1 @@
-# ---------------------------------------------------------
-# FILE PATH: src/__init__.py
-# ---------------------------------------------------------
-from . import database, coinex_client, strategy, telegram_bot, indicators, train_model, optimizer, backtester
-
-__all__ = ['database', 'coinex_client', 'strategy', 'telegram_bot', 'indicators', 'train_model', 'optimizer', 'backtester']
+from . import database
